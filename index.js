cce67bbe16979cf3ad026b37f5a21892015a2091
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-29 02:47:49 +0600

    add get api for all product and connection mongodb

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@phonegagate.m6mbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const phoneCollection = client.db("phoneGarage").collection("phone");
+
+        // GET ALL 
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = phoneCollection.find(query);
+            const phone = await cursor.toArray();
+            res.send(phone);
+        })
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
 // GET Main
 app.get('/', (req, res) => {
     res.send('Phone Garage Client Running!')
