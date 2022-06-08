b8994eba71594c5f728f254d49b7bfc9c668f02e
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-08 18:40:29 +0600

    add jwt verify function

--- index.js
+++ index.js
@@ -18,27 +18,27 @@ app.get('/', (req, res) => {
     res.send('Phone Garage Server Running!')
 });
 
-function verifyJWT(req, res, next) {
-
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: 'unauthorize access' })
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ message: 'forbidden access' })
-        }
-        req.decoded = decoded;
-        next();
-    });
-}
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@phonegagate.m6mbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
+
+    const verifyJWT = (req, res, next) => {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            return res.status(401).send({ message: 'unauthorize access' })
+        }
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            req.decoded = decoded;
+            next();
+        });
+    }
+
     try {
         await client.connect();
         const phoneCollection = client.db("phoneGarage").collection("phone");
@@ -76,7 +76,7 @@ async function run() {
         });
 
         // GET SINGLE SEARCH ID
-        app.get('/product/:id', async (req, res) => {
+        app.get('/product/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const param = { _id: ObjectId(id) };
             const phoneId = await phoneCollection.findOne(param);
